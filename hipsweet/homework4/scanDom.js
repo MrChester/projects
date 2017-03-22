ee5b08973aac6903add941d09685ed541f6ab246
Eugene Romankov <iromankov2010@gmail.com>
2017-03-22 13:29:13 +0300

    Add projects: myPortfolio, hipsweet, accordionDomEvents.

---
+++ hipsweet/homework4/scanDom.js
@@ -0,0 +1,55 @@
+/**
+ * Created by fallenSoulDev on 10.09.2016.
+ */
+var tagNameMap = new Map();
+var classNameMap = new Map();
+var textNodesMap = new Map();
+
+function scanDom(Node) {
+
+    for (var i = 0; i < Node.childNodes.length; i++) {
+        var childNode = Node.childNodes[i];
+        var tag = childNode.tagName;
+        var nodeName = childNode.nodeName;
+        var className = childNode.className;
+        if (childNode.nodeType === 1) {
+            if (!tagNameMap.has(tag)) {
+                tagNameMap.set(tag, 1);
+            }
+            else {
+                let val = tagNameMap.get(tag);
+                let count = parseInt(val) + 1;
+                tagNameMap.set(tag, count);
+            }
+            if (className !== "") {
+                if (!classNameMap.has(className)) {
+                    classNameMap.set(className, 1);
+                }
+                else {
+                    let val = classNameMap.get(className);
+                    let count = parseInt(val) + 1;
+                    classNameMap.set(className, count);
+                }
+            }
+        }
+        if (childNode.nodeType !== 3) {
+            scanDom(childNode);
+        }
+        else {
+            if (!textNodesMap.has(nodeName)) {
+                textNodesMap.set(nodeName, 1);
+            }
+            else {
+                let val = textNodesMap.get(nodeName);
+                let count = parseInt(val) + 1;
+                textNodesMap.set(nodeName, count);
+            }
+        }
+    }
+    return [tagNameMap, classNameMap, textNodesMap];
+}
+
+module.exports = scanDom;
+/**
+ * Created by fallenSoulDev on 11.10.2016.
+ */
---
+++ myPortfolio/js/trigger.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  
+  $('.contacts__trigger').on('click', function(){
+    $('.js_toggle').toggle();
+  });
+  
+});
