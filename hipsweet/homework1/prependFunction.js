bf5b04d4e78d2d3b40d70ecef60a6d27c7a12170
Eugene Romankov <iromankov2010@gmail.com>
2017-03-22 14:30:36 +0300

    Remove directories homework1, homework4, js from Git.

--- hipsweet/homework1/prependFunction.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by fallenSoulDev on 10.09.2016.
- */
-function prepend(container, newElement) {
-
-    container.insertBefore(newElement, container.firstChild)
-}
-
-module.exports = prepend;
